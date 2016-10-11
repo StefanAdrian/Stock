8cfc34509cc47455dac0b5077e0b709d5e2d5e8e
Adrian <adymartianul@gmail.com>
2016-10-11 19:17:07 +0300

    /

--- Stock/Form1.Designer.cs
+++ Stock/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Stock
 {
-    partial class Form1
+    partial class Form
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblCompany = new System.Windows.Forms.Label();
+            this.lblPeriod = new System.Windows.Forms.Label();
+            this.txtCompany = new System.Windows.Forms.TextBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCompany
+            // 
+            this.lblCompany.AutoSize = true;
+            this.lblCompany.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCompany.Location = new System.Drawing.Point(13, 13);
+            this.lblCompany.Name = "lblCompany";
+            this.lblCompany.Size = new System.Drawing.Size(67, 17);
+            this.lblCompany.TabIndex = 0;
+            this.lblCompany.Text = "Company";
+            // 
+            // lblPeriod
+            // 
+            this.lblPeriod.AutoSize = true;
+            this.lblPeriod.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeriod.Location = new System.Drawing.Point(158, 13);
+            this.lblPeriod.Name = "lblPeriod";
+            this.lblPeriod.Size = new System.Drawing.Size(49, 17);
+            this.lblPeriod.TabIndex = 0;
+            this.lblPeriod.Text = "Period";
+            // 
+            // txtCompany
+            // 
+            this.txtCompany.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCompany.Location = new System.Drawing.Point(16, 33);
+            this.txtCompany.Name = "txtCompany";
+            this.txtCompany.Size = new System.Drawing.Size(100, 20);
+            this.txtCompany.TabIndex = 1;
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "1 Day",
+            "1 Month",
+            "1 Year"});
+            this.comboBox.Location = new System.Drawing.Point(161, 33);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(120, 21);
+            this.comboBox.TabIndex = 2;
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetData.Location = new System.Drawing.Point(326, 32);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(75, 21);
+            this.btnGetData.TabIndex = 3;
+            this.btnGetData.Text = "Get data";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(16, 70);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(512, 288);
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(446, 32);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 21);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(544, 376);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnGetData);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.txtCompany);
+            this.Controls.Add(this.lblPeriod);
+            this.Controls.Add(this.lblCompany);
+            this.Name = "Form";
+            this.Text = "Stock price";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblCompany;
+        private System.Windows.Forms.Label lblPeriod;
+        private System.Windows.Forms.TextBox txtCompany;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- Stock/Form1.cs
+++ Stock/Form1.cs
@@ -10,11 +10,38 @@ using System.Windows.Forms;
 
 namespace Stock
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        public Form()
         {
             InitializeComponent();
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            var period = comboBox.Text;
+
+            switch(period)
+            {
+                case "1 Day":
+                    pictureBox.Load("http://ichart.finance.yahoo.com/b?s=" + txtCompany.Text);
+                    break;
+                case "1 Month":
+                    pictureBox.Load("http://chart.finance.yahoo.com/c/1m/d/" + txtCompany.Text);
+                    break;
+                case "1 Year":
+                    pictureBox.Load("http://chart.finance.yahoo.com/c/1y/d/" + txtCompany.Text);
+                    break;
+            }
+
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCompany.Clear();
+            pictureBox.Image = null;
+            comboBox.SelectedIndex = -1;
         }
     }
 }
